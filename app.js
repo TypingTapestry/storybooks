263382da3c1df2be520849e5f8ab3a202c769b56
Sara Swingle <sara.swingle.dev@gmail.com>
2022-09-02 10:05:43 -0600

    Upload add.hbs for ejs conversion to jbs

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const connectDB = require("./config/db");
-//These are for the user image upload - SS
+const fs = require("fs");
 const multer = require("multer");
-// const cloudinary = require("cloudinary");
-// const cloudinaryStorage = require("multer-storage-cloudinary");
-//TODO: switch the image upload ejs file to handlebars - SS
-const ejs = require("ejs");
 
 // Load Config
 dotenv.config({ path: "./config/config.env" });
@@ -25,47 +21,55 @@ connectDB();
 
 const app = express();
 
-//Set storage engine - need to change to DB instead of public folder
-const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-
-//Init upload
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1000000,
-    fileFilter: function (req, file, cb) {
-      checkFileType(file, cb);
-    },
-  },
-}).single(myImage);
-
-//Check file type
-function checkFileType(file, cb) {
-  //Allowed extentions
-  const filetypes = /jpeg|jpg|png/;
-  //Check extentions
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  //Check mimetype
-  const mimetype = filetypes.test(file.mimetype);
-  //
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: images only.");
-  }
-}
-
 // Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const imgModel = require("./model/Image");
+
+app.get("/", (req, res) => {
+  imgModel.find({}, (err, tiems) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("An error occured", err);
+    } else {
+      res.render("imagesPage", { items: items });
+    }
+  });
+});
+
+app.post("/", upload.single("image"), (req, res, next) => {
+  let obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentTYpe: "image/ong",
+    },
+  };
+  imgModel.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      res.redirect("/");
+    }
+  });
+});
 
 // Method Override
 app.use(
@@ -139,28 +143,3 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}! :)`
   )
 );
-
-//SSSSSSS - This is the image upload stuff, still working on it but wanted you guys to be able to see it
-
-//app.get("/", (req, res) => res.render("index"));
-
-// app.post("/upload", (res, req) => {
-//   upload(res, req, (err) => {
-//     if (err) {
-//       res.render("index", {
-//         msg: err,
-//       });
-//     } else {
-//       if (req.file === undefined) {
-//         res.render("index", {
-//           msg: "Error: no file selected",
-//         });
-//       } else {
-//         res.render("index", {
-//           msg: "File uploaded",
-//           file: `uploads/${req.file.filename}`,
-//         });
-//       }
-//     }
-//   });
-// });
