b244598408e622d473db2ca4091719fb100d5e30
TypingTapestry <typingtapestry@gmail.com>
2022-09-05 19:43:47 -0500

    minor tweaks

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
-const methodOverride = require("method-override");
-const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const connectDB = require("./config/db");
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const connectDB = require('./config/db');
 
 // Load Config
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 // Passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -24,50 +24,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Method Override
-app.use(
-  methodOverride(function (req, res) {
-    if (req.body && typeof req.body === "object" && "_method" in req.body) {
-      let method = req.body._method;
-      delete req.body._method;
-      return method;
-    }
-  })
-);
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    };
+}));
 
 // Logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
 
 // Handlebars Helpers
-const {
-  formatDate,
-  stripTags,
-  truncate,
-  editIcon,
-  select,
-} = require("./helpers/hbs");
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
 
 // Handlebars
-app.engine(
-  ".hbs",
-  exphbs.engine({
-    helpers: { formatDate, stripTags, truncate, editIcon, select },
-    defaultLayout: "main",
-    extname: ".hbs",
-  })
-);
-app.set("view engine", ".hbs");
+app.engine('.hbs', exphbs.engine({ helpers: { formatDate, stripTags, truncate, editIcon, select }, defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 // Sessions
-app.use(
-  session({
-    secret: "smile",
+app.use(session({
+    secret: 'smile',
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-  })
-);
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
+}));
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -75,23 +58,18 @@ app.use(passport.session());
 
 // Set Global Variable
 app.use(function (req, res, next) {
-  res.locals.user = req.user || null;
-  next();
+    res.locals.user = req.user || null;
+    next();
 });
 
 // Static Folder
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
 // Routes
-app.use("/", require("./routes/index"));
-app.use("/auth", require("./routes/auth"));
-app.use("/stories", require("./routes/stories"));
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}! :)`
-  )
-);
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}! :)`));
